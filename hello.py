7fc690b729926b9dcda58cc06ce3a3e9c1a305b6
Thiago Perrotta <tbperrotta@gmail.com>
2017-05-13 15:57:48 -0300

    MASTER SYNC

--- hello.py
+++ hello.py
@@ -1,58 +1,66 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# vim: noet
 
 from pprint import pprint
 import json
 import requests
 import urllib
 
-# Docs:
-#
-# - esporte_slug = futebol
-# - modalidade_slug = futebol_de_campo
-# - categoria_slug = profissional
-#                               /esportes/{esporte_slug}/modalidades/{modalidade_slug}/categorias/{categoria_slug}/campeonatos'
-
 HEADERS = {'token': 'hack2017-grupo3'}
 URL_BASE = 'https://api.sde.globo.com/esportes/futebol/modalidades/futebol_de_campo/categorias/profissional'
 
 
-def get_campeonatos():
+def query_campeonatos():
 	url = URL_BASE + '/campeonatos/campeonato-brasileiro/edicoes/'
-	return requests.get(url, headers=HEADERS)
+	return requests.get(url, headers=HEADERS).json()
 
-def get_campeonato_brasileiro_2017():
+def query_campeonato_brasileiro_2017():
 	url = URL_BASE + '/campeonatos/campeonato-brasileiro/edicoes/campeonato-brasileiro-2017/jogos'
-	return requests.get(url, headers=HEADERS)
+	return requests.get(url, headers=HEADERS).json()
 
-def get_equipes():
+def query_equipes():
 	url = URL_BASE + '/campeonatos/campeonato-brasileiro/edicoes/campeonato-brasileiro-2017/equipes'
-	return requests.get(url, headers=HEADERS)
+	return requests.get(url, headers=HEADERS).json()
 
-# -> List<String>
-def get_equipes_popular_name_list():
-	response = get_equipes().json()['resultados']['equipes']
+def get_equipe_by_id(id):
+	equipes = query_equipes()['resultados']['equipes']
+	for equipe in equipes:
+		if int(equipe['equipe_id']) == id:
+			return equipe
+	return None
+
+def get_equipe_by_slug(slug):
+	equipes = query_equipes()['resultados']['equipes']
+	for equipe in equipes:
+		if equipe['slug'] == slug:
+			return equipe
+	return None
+
+def get_slug_by_equipe_id(id):
+	equipes = query_equipes()['resultados']['equipes']
+	for equipe in equipes:
+		if equipe['equipe_id'] == id:
+			return equipe['slug']
+	return None
+
+def get_equipe_id_by_slug(slug):
+	equipes = query_equipes()['resultados']['equipes']
+	for equipe in equipes:
+		if equipe['slug'] == slug:
+			return equipe['equipe_id']
+	return None
+
+def get_list_of_equipes_popular_names():
+	equipes = query_equipes()['resultados']['equipes']
 	ret = []
-	for t in response:
-		ret.append(t['nome_popular'])
+	for equipe in equipes:
+		ret.append(equipe['nome_popular'])
 	return ret
 
-def get_equipe_by_name(name):
-    equipes_list = get_equipes_popular_name_list()
-    id = -1
-    for k, v in equipes_list.iteritems():
-        if name == k:
-            id = v
-    return id
-
-# (266, '2017-05-01') -> dict
-def get_next_game(team_id, date):
-	url = URL_BASE + '/campeonatos/campeonato-brasileiro/edicoes/campeonato-brasileiro-2017/jogos?equipe_id=%s&data_hora_inicial=%s' % (team_id, date)
+def get_next_game_by_equipe_id(equipe_id, initial_date):
+	url = URL_BASE + '/campeonatos/campeonato-brasileiro/edicoes/campeonato-brasileiro-2017/jogos?equipe_id=%s&data_hora_inicial=%s' % (equipe_id, initial_date)
 	response = requests.get(url, headers=HEADERS)
 	return (response.json())['resultados']['jogos'][0]
 
-# response = get_next_game(266, "2017-05-01")
-# pprint(response)
-
-# print get_equipes_popular_name_list()
-print get_equipe_by_name('Flamengo')
+pprint(get_list_of_equipes_popular_names())
