7f3ba5082eec696b15c686c384fb3d2eac668cfd
Pedro Eusébio <pedroeusebio@poli.ufrj.br>
2017-05-13 17:16:07 -0300

    update

--- user.py
+++ user.py
@@ -1,5 +1,17 @@
+import facebook
+import secrets
+
+VERSION = '/v2.9/'
+
+graph = facebook.GraphAPI(secrets.ACCESS_TOKEN)
+
 class User:
     def __init__(self, recipient_id):
         self.recipient_id = recipient_id
         self.team_slug = None
-        self.name = "Fulano"
+        self.name = self.get_name(recipient_id)
+
+    def get_name(self, recipient_id):
+        query = VERSION + str(recipient_id)
+        data = graph.request(query)
+        return data['name']
