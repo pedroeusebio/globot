b917990aec2ace8c33447ccb80cb2d4cabe5c6a9
Thiago Perrotta <tbperrotta@gmail.com>
2017-05-13 13:57:49 -0300

    increment

--- hello.py
+++ hello.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 
+import json
 import requests
 
-url = 'https://api.sde.globo.com/esportes/futebol/modalidades/futebol_de_campo/categorias/profissional/campeonatos/campeonato-brasileiro/edicoes/'
+# Docs:
+#
+# - esporte_slug = futebol
+# - modalidade_slug = futebol_de_campo
+# - categoria_slug = profissional
+#                               /esportes/{esporte_slug}/modalidades/{modalidade_slug}/categorias/{categoria_slug}/campeonatos'
 
-# url = 'https://api.sde.globo.com/esportes'
-headers = {'token': ''}
+URL_BASE = 'https://api.sde.globo.com/esportes/futebol/modalidades/futebol_de_campo/categorias/profissional'
+HEADERS = {'token': 'hack2017-grupo3'}
 
-response = requests.get(url, headers=headers).json()
+# url = URL_BASE + '/campeonatos/campeonato-brasileiro/edicoes/'
+url = URL_BASE + '/campeonatos/campeonato-brasileiro/edicoes/campeonato-brasileiro-2017/jogos'
+
+response = requests.get(url, headers=HEADERS).json()
+
 print response['resultados']
