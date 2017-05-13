f614e2306ba215ebb843f089539ca58bc5c533fe
Bernardo Amorim <contato@bamorim.com>
2017-05-13 16:23:40 -0300

    cli client basic

---
+++ cli.py
@@ -0,0 +1,12 @@
+from core.conversation import TextResponse
+from repo import ConversationRepository
+
+fake_id = 1234
+conversation_repository = ConversationRepository()
+conversation = conversation_repository.get_or_create(fake_id)
+
+while True:
+    message = input('> ')
+    response = conversation.message(message)
+    if isinstance(response, TextResponse):
+        print(response.text)
