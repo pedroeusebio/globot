e725bf42054dc0dc5ba241401cc6927bdc79b6e0
Bernardo Amorim <contato@bamorim.com>
2017-05-13 15:47:33 -0300

    Confirmation

--- core/conversation.py
+++ core/conversation.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
 from collections import namedtuple
 from core.user import User
+from fuzzywuzzy import fuzz
 
 TextResponse = namedtuple('TextResponse', 'text')
 
@@ -8,6 +8,7 @@ class State:
     INIT = 0
     ONBOARDING = 1
     ASKING_TEAM = 2
+    CONFIRMING_TEAM = 3
 
 class Conversation:
     def __init__(self, recipient_id):
@@ -24,6 +25,9 @@ class Conversation:
         if self.state == State.ASKING_TEAM:
             return self.asking_team(msg)
 
+        if self.state == State.CONFIRMING_TEAM:
+            return self.confirming_team(msg)
+
         return self.default(msg)
 
     def onboarding(self, msg):
@@ -33,8 +37,15 @@ class Conversation:
     def asking_team(self, msg):
         # TODO : Use api
         self.user.team_slug = msg
-        self.state = State.INIT
-        return TextResponse("Irado! Seu time é o {}".format(self.user.team_slug))
+        self.state = State.CONFIRMING_TEAM
+        return TextResponse("Irado! Seu time é o {}, certo?".format(self.user.team_slug))
+
+    def confirming_team(self, msg):
+        if fuzz.ratio(msg, 'sim') > 49:
+            self.state = State.INIT
+            return TextResponse("Show! Então é isso.")
+
+        return self.onboarding(msg)
 
     def default(self, msg):
         return TextResponse("Não sei o que dizer HAHAHA. Só vamo {}!".format(self.user.team_slug))
